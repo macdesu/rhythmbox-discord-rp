cc35b269c146b24cb12988c01bc6ee0ae04a50da
MacDesu <32233686+macdesu@users.noreply.github.com>
2019-02-08 19:52:32 +0100

    i don't even remember what i did

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 PLUGIN_PATH="$HOME/.local/share/rhythmbox/plugins/rbdrp"
 
 mkdir -p $PLUGIN_PATH
-cp rbdrp.py rbdrp.plugin -t $PLUGIN_PATH
+cp *.py rbdrp.plugin -t $PLUGIN_PATH
--- rbdrp.py
+++ rbdrp.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 import os, time
 from pypresence import Presence
+from gi.repository import GObject, RB, Peas
 
-client_id = "543459024390324245"
-RPC = Presence(client_id)
-RPC.connect()
-
-while True:
-	time.sleep(1)
-	artist = os.popen("rhythmbox-client --print-playing-format=\"%ta\"").read()
-	title = os.popen("rhythmbox-client --print-playing-format=\"%tt\"").read()
-	start = os.popen("rhythmbox-client --print-playing-format=\"%te\"").read()
-	end = os.popen("rhythmbox-client --print-playing-format=\"%td\"").read()
-	RPC.update(details=artist, state=title)
+def discord():
+	client_id = "543459024390324245"
+	RPC = Presence(client_id)
+	RPC.connect()
+	while True:
+		time.sleep(1)
+		artist = os.popen("rhythmbox-client --print-playing-format=\"%ta\"").read()
+		title = os.popen("rhythmbox-client --print-playing-format=\"%tt\"").read()
+		timestamp = os.popen("rhythmbox-client --print-playing-format=\"%te / %td\"").read()
+		RPC.update(details=artist, state=title + timestamp)
