49f05b29d4aec98b4c967da29af2724db802cacf
MacDesu <32233686+macdesu@users.noreply.github.com>
2019-02-08 20:21:12 +0100

    dev branch added

--- rbdrp.py
+++ rbdrp.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python
 import os, time
 from pypresence import Presence
-from gi.repository import GObject, RB, Peas
+# from gi.repository import GObject, RB, Peas
 
 def discord():
+	old_timestamp = ''
 	client_id = "543459024390324245"
 	RPC = Presence(client_id)
 	RPC.connect()
+
 	while True:
 		time.sleep(1)
 		artist = os.popen("rhythmbox-client --print-playing-format=\"%ta\"").read()
 		title = os.popen("rhythmbox-client --print-playing-format=\"%tt\"").read()
-		timestamp = os.popen("rhythmbox-client --print-playing-format=\"%te / %td\"").read()
-		RPC.update(details=artist, state=title + timestamp)
+		timestamp = os.popen("rhythmbox-client --print-playing-format=\"(%te / %td)\"").read()
+		if timestamp != old_timestamp:
+			old_timestamp = timestamp
+			small_image = "play"
+		else:
+			small_image = "pause"
+		RPC.update(details=artist, state=title + timestamp, large_image="rhythmbox", small_image=small_image)
+
+discord()
