711b84ce998f32e9b1f3ea50b7f5d5cabae6cf56
MacDesu <32233686+macdesu@users.noreply.github.com>
2019-02-08 23:37:12 +0100

    commented code

--- rbdrp.py
+++ rbdrp.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python
 import os, time
 from pypresence import Presence
-# from gi.repository import GObject, RB, Peas
 
-def discord():
-	old_timestamp = '0:00'
-	client_id = "543459024390324245"
-	RPC = Presence(client_id)
-	RPC.connect()
+old_timestamp = '0:00'
+client_id = "543459024390324245"
+RPC = Presence(client_id)
+RPC.connect()
 
-	while True:
-		time.sleep(1)
-		artist = os.popen("rhythmbox-client --print-playing-format=\"%ta\"").read()
-		title = os.popen("rhythmbox-client --print-playing-format=\"%tt\"").read()
-		timestamp = os.popen("rhythmbox-client --print-playing-format=\"(%te / %td)\"").read()
-		if timestamp != old_timestamp:
-			old_timestamp = timestamp
-			small_image = "play-circle"
-		else:
-			small_image = "pause-circle"
-			timestamp = " (Paused)"
-		if len(artist) < 2:
-			artist = "   "
-		if len(title) < 2:
-			title = "IDLE"
-		RPC.update(details=artist, state=title + timestamp, large_image="rhythmbox", small_image=small_image)
+while True:
+	time.sleep(1)
+	artist = os.popen("rhythmbox-client --print-playing-format=\"%ta\"").read()
+	title = os.popen("rhythmbox-client --print-playing-format=\"%tt\"").read()
+	timestamp = os.popen("rhythmbox-client --print-playing-format=\"(%te / %td)\"").read()
 
-discord()
+	# play / pause
+	if timestamp != old_timestamp:
+		old_timestamp = timestamp
+		small_image = "play-circle"
+	else:
+		small_image = "pause-circle"
+		timestamp = " (Paused)"
+
+	# prevent crash when stopped
+	if len(artist) < 2:
+		artist = "   "
+	if len(title) < 2:
+		title = "IDLE"
+
+	RPC.update(details=artist, state=title + timestamp, large_image="rhythmbox", small_image=small_image)
